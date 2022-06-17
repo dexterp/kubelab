97dbdaaa3450d1a1ccbdb4e8d61652e0e77e1d0e
Dexter Plameras <dexterp@gmail.com>
2022-06-17 20:54:59 +1000

    Fix adding of control planes
    - Renamed ansible variables for VIP and port
    - Make adding of control plane nodes robust
    - Disable skip host messages

--- ansible/roles/kube-control-plane/files/usr/local/sbin/kube-gen-add-control.sh
+++ ansible/roles/kube-control-plane/files/usr/local/sbin/kube-gen-add-control.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
 umask 077
-certsecret=$(kubeadm init phase upload-certs --upload-certs | tail -1)
-command="$(kubeadm token create --ttl 2h --print-join-command) --control-plane --certificate-key ${certsecret}"
 
-cat <<EOF
+outfile=$1
+tlssecret=$(kubeadm init phase upload-certs --upload-certs | tail -1)
+command="$(kubeadm token create --ttl 2h --print-join-command) --control-plane --certificate-key ${tlssecret}"
+
+tmpfile=$(mktemp /tmp/kube-add-conrol.XXXXXX)
+
+cat > $tmpfile <<EOF
 #!/usr/bin/env bash
 if [[ ! -f /etc/kubernetes/admin.conf ]]
 then
   $command
 fi
 EOF
+
+mv $tmpfile $outfile
+
+at now + 1hour <<< "rm -f ${outfile}"
